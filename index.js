1ba314a711636b656401ffa2a4a205e5ca88041f
Othman <othman.douz07@gmail.com>
2023-05-19 19:23:36 +0200

    test

--- index.js
+++ index.js
@@ -1,68 +1,32 @@
-const pokemonListElement = document.getElementById('pokemonList');
-const pokemonDetailsElement = document.getElementById('pokemonDetails');
-const searchInput = document.getElementById('searchInput');
+const fetchPokemon = () => {
 
-let pokemonList = [];
+    const promises = [];
+    for (let i = 1; i < 150; i++){
+    const url = 'https://pokeapi.co/api/v2/pokemon/${i}';
+    promises.push(festch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then( results => {
+         const pokemon = results.map( result => ({
+                name: data.name,
+                id: data.id,
+                image: data.sprites['front-_default'],
+                type: data.types.map((type) => type.name).join(',')
+         }));
+         displayPokemon(pokemon);
+        });
+};
 
-// ...
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map ( pokeman => `
+    <li>
+        <img src="${pokemon.image}"/>
+        <h2>${pokeman.id}. ${pokemon.name}</h2>
+        <p>Type: ${pokeman.type}</p>
+    </li>
+    `)
+    pokedex.innerHTML = pokemonHTMLString;
 
-// Fonction pour récupérer les détails d'un Pokémon depuis l'API PokeAPI
-async function fetchPokemonDetails(pokemon) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-    const data = await response.json();
-    displayPokemonDetails(data);
-  }
-// ...
-  
+};
 
-// Fonction pour afficher la liste des Pokémon
-function displayPokemonList() {
-  pokemonListElement.innerHTML = '';
-
-  pokemonList.forEach(pokemon => {
-    const pokemonCard = document.createElement('div');
-    pokemonCard.classList.add('pokemon-card');
-    pokemonCard.textContent = `${pokemon.name} (#${pokemon.id})`;
-    pokemonCard.addEventListener('click', () => fetchPokemonDetails(pokemon));
-    pokemonListElement.appendChild(pokemonCard);
-   
-  });
-}
-
-// Fonction pour récupérer les détails d'un Pokémon depuis l'API PokeAPI
-async function fetchPokemonDetails(pokemon) {
-  const response = await fetch(pokemon.url);
-  const data = await response.json();
-  displayPokemonDetails(data);
-}
-
-// Fonction pour afficher les détails d'un Pokémon
-function displayPokemonDetails(pokemon) {
-  pokemonDetailsElement.innerHTML = `
-    <h2>${pokemon.name} (#${pokemon.id})</h2>
-    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-    <p>Type: ${pokemon.types.map(type => type.type.name).join(', ')}</p>
-    <p>Stats:</p>
-    <ul>
-      ${pokemon.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
-    </ul>
-  `;
-}
-
-// Fonction pour effectuer une recherche de Pokémon
-function searchPokemon() {
-  const searchTerm = searchInput.value.toLowerCase();
-  const filteredPokemonList = pokemonList.filter(pokemon =>
-    pokemon)}
-
-// Utilisez l'API Fetch pour envoyer une requête GET à l'endpoint des types
-fetch('https://pokeapi.co/api/v2/type')
-  .then(response => response.json())
-  .then(data => {
-    // Extraire les noms des types de Pokémon
-    const types = data.results.map(type => type.name);
-    console.log(types); // Affiche les noms des types de Pokémon
-  })
-  .catch(error => console.log(error));
-
-
+fetchPokemon();
