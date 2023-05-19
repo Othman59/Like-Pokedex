b1ee94e2afe68acf72059d5b6642153de9356efa
Othman <othman.douz07@gmail.com>
2023-05-19 20:19:55 +0200

    test

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-const fetchPokemon = () => {
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const pokemonList = document.getElementById('pokemonList');
+const pokemonDetails = document.getElementById('pokemonDetails');
 
-    const promises = [];
-    for (let i = 1; i < 150; i++){
-    const url = 'https://pokeapi.co/api/v2/pokemon/${i}';
-    promises.push(festch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then( results => {
-         const pokemon = results.map( result => ({
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front-_default'],
-                type: data.types.map((type) => type.name).join(',')
-         }));
-         displayPokemon(pokemon);
-        });
-};
+// Fonction pour obtenir la liste des Pokémon depuis l'API PokeAPI
+async function getPokemonList() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+  const data = await response.json();
 
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon.map ( pokeman => `
-    <li>
-        <img src="${pokemon.image}"/>
-        <h2>${pokeman.id}. ${pokemon.name}</h2>
-        <p>Type: ${pokeman.type}</p>
-    </li>
-    `)
-    pokedex.innerHTML = pokemonHTMLString;
+  const pokemon = data.results;
+  pokemon.forEach(p => {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('pokemon-card');
+    pokemonCard.innerText = `${p.name} (#${getPokemonIdFromUrl(p.url)})`;
 
-};
+    pokemonCard.addEventListener('click', () => {
+      getPokemonDetails(p.name);
+    });
 
-fetchPokemon();
+    pokemonList.appendChild(pokemonCard);
+  });
+}
+
+// Fonction pour obtenir les détails d'un Pokémon depuis l'API PokeAPI
+async function getPokemonDetails(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
+  
+    const pokemonHtml = `
+      <h2>${data.name} (#${data.id})</h2>
+      <img src="${data.sprites.front_default}" alt="${data.name}">
+      <table>
+        <tr>
+          <th>Statistique</th>
+          <th>Valeur</th>
+        </tr>
+        ${data.stats
+          .map(
+            stat =>
+              `<tr><td>${stat.stat.name}</td><td>${stat.base_stat}</td></tr>`
+          )
+          .join('')}
+      </table>
+    `;
+  
+    pokemonDetails.innerHTML = pokemonHtml;
+  }
+  
