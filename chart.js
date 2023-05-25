755dcf3a043a56245f5024845baf9da0a4811959
Othman <othman.douz07@gmail.com>
2023-05-25 09:46:58 +0200

    test

---
+++ chart.js
@@ -0,0 +1,9 @@
+const ctx = document.getElementById('myChart');
+
+var pokemonStats = [
+    { name: "Pikachu", hp: 35, attack: 55, defense: 40, speed: 90 },
+    { name: "Bulbasaur", hp: 45, attack: 49, defense: 49, speed: 45 },
+    { name: "Charizard", hp: 78, attack: 84, defense: 78, speed: 100 },
+    { name: "Squirtle", hp: 44, attack: 48, defense: 65, speed: 43 },
+    { name: "Gengar", hp: 60, attack: 65, defense: 60, speed: 110 },
+];
